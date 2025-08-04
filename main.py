8f91e85940d6743e9e3cea8001d425ba89897397
proDream <sushkoos@gmail.com>
2025-08-04 13:20:31 +0400

    CI/CD: Deploy

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 import os
 
 from aiogram import Dispatcher, Bot
+from aiogram.filters import Command
 from aiogram.types import Message
 from dotenv import load_dotenv
 
@@ -10,10 +11,14 @@ load_dotenv()
 async def send_message(message: Message) -> None:
     await message.answer(text="Hello World!")
 
+async def send_sticker(message: Message) -> None:
+    await message.answer_sticker(sticker="CAACAgIAAxkBAAEKbW1lGVW1I6zFVLyovwo2rSgIt1l35QADJQACYp0ISWYMy8-mubjIMAQ")
+
 async def start():
     bot = Bot(token=os.getenv('BOT_TOKEN'))
     dp = Dispatcher()
 
+    dp.message.register(send_sticker, Command(commands="/test"))
     dp.message.register(send_message)
 
     try:
